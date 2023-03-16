a431f1725095b534059895b0b38734846c862e76
kevinllo <kevingrande2002@gmail.com>
2023-03-16 14:56:13 -0600

    ADD: RootComponent layout

--- src/components/Root/RootComponent.tsx
+++ src/components/Root/RootComponent.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import Footer from '../Footer/Footer'
+import Navbar from '../Navbar/Navbar'
+import { Outlet } from 'react-router-dom';
 
 export default function RootComponent() {
   return (
-    <div>Root</div>
+    <>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </>
   )
 }
