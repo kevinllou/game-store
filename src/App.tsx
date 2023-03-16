829c9a407a4a8844efdcd1397f6d5173364caea2
kevinllo <kevingrande2002@gmail.com>
2023-03-16 14:57:57 -0600

    DELETE: Home page from routes

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,11 @@ import './App.scss';
 import RootComponent from './components/Root/RootComponent';
 import DetailGame from './pages/DetailsGame/DetailGame';
 import Games from './pages/Games/Games';
-import Home from './pages/Home/Home';
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<RootComponent />}>
-          <Route path='home' index={true} element={<Home />} />
           <Route path='games' element={<Games />} />
           <Route path='detail-game' element={<DetailGame />} />
         </Route>
