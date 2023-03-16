d382445eea88fdf58c83e39ee2ddea24f6519ff0
kevinllo <kevingrande2002@gmail.com>
2023-03-16 14:47:49 -0600

    ADD:  router logic to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.scss';
+import RootComponent from './components/Root/RootComponent';
+import DetailGame from './pages/DetailsGame/DetailGame';
+import Games from './pages/Games/Games';
+import Home from './pages/Home/Home';
 function App() {
   return (
-    <div className="App">
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<RootComponent />}>
+          <Route path='home' index={true} element={<Home />} />
+          <Route path='games' element={<Games />} />
+          <Route path='detail-game' element={<DetailGame />} />
+        </Route>
 
-    </div>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
