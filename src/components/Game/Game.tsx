9fa4763932ca431a1bbba4a1885bc0a1be185911
kevinllo <kevingrande2002@gmail.com>
2023-03-17 14:46:20 -0600

    ADD: Game component basic structure

---
+++ src/components/Game/Game.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Game() {
+    return (
+        <section className="cards">
+            <div className="cards__grid">
+                
+            </div>
+        </section>
+    )
+}
