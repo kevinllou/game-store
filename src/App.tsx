e8fef88e8a22c678bd56d1e8306178ed516a9bbe
kevinllo <kevingrande2002@gmail.com>
2023-03-17 11:53:06 -0600

    ADD: Home page to routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.scss';
 import RootComponent from './components/Root/RootComponent';
+import Home from './pages/Home/Home';
 import DetailGame from './pages/DetailsGame/DetailGame';
 import Games from './pages/Games/Games';
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<RootComponent />}>
+          <Route index element={<Home />} />
           <Route path='games' element={<Games />} />
           <Route path='games/:id' element={<DetailGame />} />
         </Route>
