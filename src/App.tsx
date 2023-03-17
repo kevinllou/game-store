c9ca01aab9707c3dea8e6ecb6a50797aa7c0a2f3
kevinllo <kevingrande2002@gmail.com>
2023-03-17 11:40:06 -0600

    UPDATE: game/id route to render detail page

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
       <Routes>
         <Route path='/' element={<RootComponent />}>
           <Route path='games' element={<Games />} />
-          <Route path='detail-game' element={<DetailGame />} />
+          <Route path='games/:id' element={<DetailGame />} />
         </Route>
 
       </Routes>
