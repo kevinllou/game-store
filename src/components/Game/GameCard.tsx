6571dad472c069c8b8741f90b79f664bac63fbc3
kevinllo <kevingrande2002@gmail.com>
2023-03-20 11:52:55 -0600

    UPDATE: change route in details pages when clicked in cards

--- src/components/Game/GameCard.tsx
+++ src/components/Game/GameCard.tsx
@@ -2,7 +2,7 @@ import IGames from '../../interfaces/IGames';
 import { Link } from 'react-router-dom';
 export default function GameCard({ id, background_image, name }: IGames) {
 	return (
-		<Link to={id.toString()}>
+		<Link to={`/games/${id.toString()}`}>
 			<article className="cards__gridItems">
 				<div className="cards__gridItemsImg">
 					<img src={background_image} alt={name} />
