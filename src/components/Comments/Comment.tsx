a848b1f5742d74e4bfdc93efee0b761394cb41b4
kevinllo <kevingrande2002@gmail.com>
2023-03-20 17:07:21 -0600

    FIX: users name showing in comments

--- src/components/Comments/Comment.tsx
+++ src/components/Comments/Comment.tsx
@@ -1,27 +1,36 @@
 import { ENDPOINTS } from '../../constants/endpoints';
 import { useFetch } from '../../hooks/useFetch';
 import { IComments } from '../../interfaces/IComments';
+import { IUsers } from '../../interfaces/IUsers';
 import Spinner from '../Spinner/Spinner';
 
 interface CommentProps {
-    gameId: string | undefined;
+	gameId: string | undefined;
 }
 export default function Comment({ gameId }: CommentProps) {
-	const { state, data, error } = useFetch<IComments[]>(`${ENDPOINTS.GET_COMMENTS}?gameId=${gameId}`);
+	const { state: stateComments, data: comments, error: errorComments } = useFetch<IComments[]>(`${ENDPOINTS.GET_COMMENTS}?gameId=${gameId}`);
+	const { state: stateUsers, data: users, error: usersError } = useFetch<IUsers[]>(`${ENDPOINTS.GET_USERS}`);
 
-	if (state === 'loading') return <Spinner />;
-	if (error) return <p style={{ color: 'white' }}>There was an error</p>;
+	if (stateComments === 'loading' && stateUsers === 'loading') return <Spinner />;
+	if (errorComments && usersError) return <p style={{ color: 'white' }}>There was an error</p>;
 	return (
 		<div className="detail__comments">
 			<h1>Comments</h1>
 
-			{data?.map(({ userId, comment }, id) => {
+			{comments?.map(({ userId, comment }, id) => {
 				return (
 					<div className="detail__commentsBody" key={id}>
 						<div className="detail__commentBodyUserInfo">
-							<p>{userId} <i className="fa-solid fa-user"></i></p>
+							<i className="fa-solid fa-user"></i>
 						</div>
-						<p>{comment}</p>
+						<div className="detail__commentsUser">
+							{users?.map(({ id, name }) => {
+								if (userId === id) {
+									return <p key={id}>{name} </p>;
+								}
+							})}
+							<p>{comment}</p>
+						</div>
 					</div>
 				);
 			})}
