87c0a19d5dcb9dea89698f3427e83d1692122c01
kevinllo <kevingrande2002@gmail.com>
2023-03-20 10:01:26 -0600

    ADD: detial game shows game by its id

---
+++ src/interfaces/IDetailGame.ts
@@ -0,0 +1,8 @@
+export interface IDetailGame{
+    id: number;
+    name: string;
+    released: string;
+    background_image: string;
+    rating: number;
+    description_raw: string;
+}
--- src/pages/DetailsGame/DetailGame.tsx
+++ src/pages/DetailsGame/DetailGame.tsx
@@ -1,7 +1,34 @@
 import './DetailGame.scss';
+import { useParams } from 'react-router-dom';
+import { useFetch } from '../../hooks/useFetch';
+import { IDetailGame } from '../../interfaces/IDetailGame';
+import { ENDPOINTS } from '../../constants/endpoints';
+import Spinner from '../../components/Spinner/Spinner';
 
 export default function DetailGame() {
+	const { id } = useParams();
+	const { state, data, error } = useFetch<IDetailGame>(`${ENDPOINTS.GET_GAME_BY_ID}/${id}?key=${process.env.REACT_APP_API_KEY}`);
+
+	if (state === 'loading') return <Spinner />;
+	if (error) return <p style={{ color: 'white' }}>There was an error</p>;
 	return (
-		<div>DetailGame</div>
+		<section className="detail">
+			<div className="detail__card">
+				<div className="detail__cardImg">
+					<img src={data?.background_image} alt={data?.name} />
+				</div>
+				<div className="detail__cardBody">
+					<div className="detail__cardBodyInfo">
+						<p className='detail__cardBodyTitle'>{data?.name}</p>
+						<p className='detail__released'>Realesed: <span>{data?.released}</span></p>
+						<p className='detail__cardDescription'>{data?.description_raw}</p>
+					</div>
+					<div className="detail__cardBodyBottom">
+						<span>{data?.rating}<i className="fa-sharp fa-solid fa-star"></i> </span>
+					</div>
+				</div>
+			</div>
+		</section >
+
 	);
 }
