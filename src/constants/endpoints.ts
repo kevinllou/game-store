bb8df28b056e6a0a18cd78fafe6e006b8edb0893
kevinllo <kevingrande2002@gmail.com>
2023-03-19 20:08:33 -0600

    ADD: endpoints constants

---
+++ src/constants/endpoints.ts
@@ -0,0 +1,8 @@
+export const ENDPOINTS = {
+    GET_GAMES: `https://api.rawg.io/api/games?key=${process.env.REACT_APP_API_KEY}`,
+    GET_GAME_BY_ID: `https://api.rawg.io/api/games`,
+    GET_GENRES: `https://api.rawg.io/api/genres?key=${process.env.REACT_APP_API_KEY}`,
+    GET_USERS: `https://rhinestone-shimmering-broom.glitch.me/users`,
+    GET_COMMENTS: `https://rhinestone-shimmering-broom.glitch.me/comments`,
+
+}
