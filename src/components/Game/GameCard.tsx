42491d25fa7fb79e676e6d66ab409f23093bff9a
kevinllo <kevingrande2002@gmail.com>
2023-03-20 09:33:26 -0600

    ADD: link to card details by id

--- src/components/Game/GameCard.tsx
+++ src/components/Game/GameCard.tsx
@@ -1,11 +1,14 @@
 import IGames from '../../interfaces/IGames';
-export default function GameCard({ background_image, name }: IGames) {
+import { Link } from 'react-router-dom';
+export default function GameCard({ id, background_image, name }: IGames) {
 	return (
-		<article className="cards__gridItems">
-			<div className="cards__gridItemsImg">
-				<img src={background_image} alt={name} />
-			</div>
-			<p>{name}</p>
-		</article>
+		<Link to={id.toString()}>
+			<article className="cards__gridItems">
+				<div className="cards__gridItemsImg">
+					<img src={background_image} alt={name} />
+				</div>
+				<p>{name}</p>
+			</article>
+		</Link>
 	);
 }
